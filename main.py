b3b60e1790389469fed79276611a8e4bbc124329
Alex25UM <alexander.al@maine.edu>
2025-11-25 19:04:40 -0500

    Add files via upload
    The first function I decided to use for hashing includes a super simple index that simply takes the integer value of each letter in a string (ASCII) and adds it together.

---
+++ main.py
@@ -0,0 +1,82 @@
+#Author: Alexander Al Dahhan
+#Date: November 25th, 2025
+#Collaborators: None
+#Assignment: Hash table
+
+#Copy pasted from the brightspace file
+import csv
+import time
+
+class DataItem:
+    def __init__(self, line):
+        self.movie_name = line[0]
+        self.genre = line[1]
+        self.release_date = line[2]
+        self.director = line[3]
+        self.revenue = line[4]
+        self.rating = line[5]
+        self.min_duration = line[6]
+        self.production_company = line[7]
+        self.quote = line[8]
+        pass
+
+def hashFunction1(stringData):
+    index = 0
+    for letter in stringData:
+        index = index + ord(letter)
+
+    return index
+
+size = 10000
+hashTitleTable = [None] * size
+hashQuoteTable = [None] * size
+
+file = "MOCK_DATA.csv"
+counter = 0
+
+startTime1 = time.time()
+startTime2 = time.time()
+
+titleColissions = 0
+quoteColissions = 0
+with open(file, 'r', newline='',  encoding="utf8") as csvfile:
+    print("Opening")
+    reader = csv.reader(csvfile)
+    for row in reader:
+        if counter != 0:
+            movie = DataItem(row)
+
+            titleKey = hashFunction1(movie.movie_name)
+            titleIndx = titleKey % size #just keeps the index within the bounds of the table
+
+            while hashTitleTable[titleIndx] is not None:
+                titleColissions += 1
+                titleIndx = (titleIndx + 1) % size
+
+            hashTitleTable[titleIndx] = movie
+
+            quoteKey = hashFunction1(movie.quote)
+            quoteIndx = quoteKey % size
+
+            while hashQuoteTable[quoteIndx] is not None:
+                quoteColissions += 1
+                quoteIndx = (quoteIndx + 1) % size
+
+            hashQuoteTable[quoteIndx] = movie
+        counter += 1
+        if counter == size:
+            break
+
+endTime1 = time.time()
+endTime2 = time.time()
+
+title_wasted = hashTitleTable.count(None)
+quote_wasted = hashQuoteTable.count(None)
+
+print("Rows inserted:", counter)
+print("Title Hash Table colissions:", titleColissions)
+print("Quote Hash Table colissions:", quoteColissions)
+print("Title empty spots:", title_wasted)
+print("Quote empty spots:", quote_wasted)
+print("Title timer:", abs(startTime1 - endTime1), "seconds")
+print("Quote timer:", abs(startTime2 - endTime2), "seconds")
